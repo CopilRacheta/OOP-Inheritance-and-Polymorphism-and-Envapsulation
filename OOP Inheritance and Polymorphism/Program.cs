56868e9f02f0cfe71f463a2dbedc51c241727fe5
CopilRacheta <m.carbunariu@yahoo.com>
2022-11-29 14:32:30 +0000

    Add project files.

---
+++ OOP Inheritance and Polymorphism/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace OOP_Inheritance_and_Polymorphism
+{
+    internal partial class Inheritance_and_Polymorphism
+    {
+        static void Main(string[] args)
+        {
+
+            //Encapsulation is the procces to make sure that the classes are able to edit data within a class
+
+            // using cw and tab tab will create Console.WriteLine();
+
+            //Using Car Class
+            CAR myCar = new CAR("ASHD32", "Green", "Alpha", "Romeo", 4);
+            Console.WriteLine(myCar);
+
+            myCar.setRegistration("-21312");
+            myCar.setColour("Blue");
+            myCar.setModel("Bingo");
+            myCar.setMake("BMW");
+            myCar.setNoOfWeels(8);
+            Console.WriteLine(myCar);
+
+
+            //Using Vehicle Class
+            Vehicle truck = new Vehicle("ASFIH2", "M4", "BMW", "Black", 4);
+            Console.WriteLine(truck);
+            truck.Registration = "SGEW12";
+            truck.Model = "M2";
+            truck.Make = "Audi";
+            truck.Colour = "Red";
+            truck.NoOfWheels = 5;
+            Console.WriteLine(truck);
+
+
+            //Using Van Class
+            Van va1 = new Van("REGS12", "v1", "volvo", "Silver", 4);
+            Console.WriteLine(va1);
+        }
+
+    }
+}
---
+++ OOP Inheritance and Polymorphism/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Inheritance_and_Polymorphism
+{
+    internal class Vehicle
+    {
+        //Auto-gen Properties
+        public string Registration { get; set; }//use prop tab tab to create this line of code
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public string Colour { get; set; }
+        public int NoOfWheels { get; set; }
+
+        public Vehicle(string registration, string model, string make, string colour, int noOfWheels)//Using control and . and then you can select create constructor
+        {
+            Registration = registration;
+            Model = model;
+            Make = make;
+            Colour = colour;
+            NoOfWheels = noOfWheels;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"\n======================================" +
+                $"\nCar reg: {Registration}" +
+                $"\nCar colour: {Colour}" +
+                $"\nCar model: {Model}" +
+                $"\nCar make: {Make}" +
+                $"\nCar number of weels: {NoOfWheels}" +
+                $"\n======================================";
+        }
+    }
+}
